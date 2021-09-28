b9c9e7f43064fd3f47d6eb06d77f93ee43d4dfe2
Kyle Martinez <kyletmartinez@gmail.com>
2021-09-27 21:02:25 -0700

    Initial commit

---
+++ scripts/Copy Composition Work Area.jsx
@@ -0,0 +1,43 @@
+/**
+ * @title Copy Composition Work Area
+ * @version 1.2
+ * @author Kyle Martinez <www.kyle-martinez.com>
+ *
+ * @description Copy the Work Area from the active composition. Hold the ALT key to paste the copied
+ * Work Area to the active composition.
+ *
+ * @license This script is provided "as is," without warranty of any kind,
+ * expressed or implied. In no event shall the author be held liable for any
+ * damages arising in any way from the use of this script
+ *
+ * In other words, I'm just trying to help make life as an animator easier
+ * "A rising tide lifts all boats." - John F. Kennedy, 1963
+ */
+
+(function() {
+    function copyWorkAreaFromComp(comp) {
+        app.beginUndoGroup("Copy Work Area");
+        app.settings.saveSetting("KM Copy Work Area", "Start", comp.workAreaStart);
+        app.settings.saveSetting("KM Copy Work Area", "Duration", comp.workAreaDuration);
+        app.preferences.saveToDisk();
+        app.endUndoGroup();
+    }
+
+    function pasteWorkAreaToComp(comp) {
+        if (app.settings.haveSetting("KM Copy Work Area", "Start") === true) {
+            app.beginUndoGroup("Paste Work Area");
+            comp.workAreaStart = app.settings.getSetting("KM Copy Work Area", "Start");
+            comp.workAreaDuration = app.settings.getSetting("KM Copy Work Area", "Duration");
+            app.endUndoGroup();
+        } 
+    }
+
+    var comp = app.project.activeItem;
+    if (comp !== null && (comp instanceof CompItem)) {
+        if (ScriptUI.environment.keyboardState.altKey) {
+            pasteWorkAreaToComp(comp);
+        } else {
+            copyWorkAreaFromComp(comp);
+        }
+    }
+})();
