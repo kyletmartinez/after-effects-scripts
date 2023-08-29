a42dcf3cb6d5655eeecb527de0dd7ec6e920df17
Kyle Martinez <kyletmartinez@gmail.com>
2023-08-29 13:55:14 -0700

    Initial commit

---
+++ scripts/Add Markers At Out Points.jsx
@@ -0,0 +1,28 @@
+/**
+ * @title Add Marker(s) At Out Point(s)
+ * @version 1.0
+ * @author Kyle Martinez <www.kyle-martinez.com>
+ *
+ * @description Add a composition marker at each outpoint of every layer in the composition.
+ *
+ * @license This script is provided "as is," without warranty of any kind, expressed or implied. In
+ * no event shall the author be held liable for any damages arising in any way from the use of this
+ * script.
+ *
+ * In other words, I'm just trying to help make life as an animator easier
+ * "A rising tide lifts all boats." - John F. Kennedy, 1963
+ */
+
+(function() {
+    app.beginUndoGroup("Add Marker(s) At Out Point(s)");
+    var comp = app.project.activeItem;
+    var layers = comp.layers;
+    var numLayers = layers.length;
+    for (var l = 1; l < numLayers; l++) {
+        var layer = layers[l];
+        var time = layer.outPoint;
+        var marker = new MarkerValue("");
+        comp.markerProperty.setValueAtTime(time, marker);
+    }
+    app.endUndoGroup();
+})()
---
+++ scripts/Change Nested Composition Resolution.jsx
@@ -0,0 +1,40 @@
+/**
+ * @title Change Nested Composition Resolution
+ * @version 1.0
+ * @author Kyle Martinez <www.kyle-martinez.com>
+ *
+ * @description Change the composition resolution factor. All nested precomps
+ * will be affected as well (and precomps within those precomps and so on).
+ *
+ * @license This script is provided "as is," without warranty of any kind, expressed or implied. In
+ * no event shall the author be held liable for any damages arising in any way from the use of this
+ * script.
+ *
+ * In other words, I'm just trying to help make life as an animator easier
+ * "A rising tide lifts all boats." - John F. Kennedy, 1963
+ */
+
+(function() {
+    function changeCompResolution(comp, resolution) {
+        comp.resolutionFactor = [resolution, resolution];
+        var layers = comp.layers;
+        var numLayers = layers.length;
+        for (var l = 1; l <= numLayers; l++) {
+            var layer = layers[l];
+            if (layer.source instanceof CompItem) {
+                changeCompResolution(layer.source, resolution);
+            }
+        }
+    }
+
+    app.beginUndoGroup("Change Nested Composition Resolution");
+    var newResolutionString = prompt("New Resolution (1, 2, 3, or 4)", "");
+    if (newResolutionString !== null && newResolutionString.length > 0) {
+        var newResolutionInt = parseInt(newResolutionString);
+        if (isNaN(newResolutionInt) === false) {
+            var comp = app.project.activeItem;
+            changeCompResolution(comp, newResolutionInt);
+        }
+    }
+    app.endUndoGroup();
+})()
