69bf0bebfb2b4fdc9e25c14b835e67c35b056ce4
Kyle Martinez <kyletmartinez@gmail.com>
2023-11-01 11:11:21 -0700

    Update file name output

--- scripts/Save Frame as PNG.jsx
+++ scripts/Save Frame as PNG.jsx
@@ -1,11 +1,12 @@
 /**
  * @title Save Frame as PNG
- * @version 1.1
+ * @version 1.2
  * @author Kyle Martinez <www.kyle-martinez.com>
  *
- * @description Save the current frame as a PNG to the desktop. This does support transparency. The
- * image is not amazing quality. Frames from the same composition and time will overwrite without a
- * warning.
+ * @description Save the current frame as a PNG to the desktop. This does support transparency but
+ * the image is not amazing quality.
+ *
+ * Output will match the following format: "Comp Name YYYY-MM-DD HH.MM.SS AM.png"
  *
  * @license This script is provided "as is," without warranty of any kind, expressed or implied. In
  * no event shall the author be held liable for any damages arising in any way from the use of this
@@ -16,9 +17,33 @@
  */
 
 (function() {
+    function getDate(date) {
+        var year = date.getFullYear().toString();
+        var month = date.getMonth().toString();
+        var day = date.getDate().toString();
+        if (month.length == 1) {month = "0" + month;}
+        if (day.length == 1) {day = "0" + day;}
+        return year + "-" + month + "-" + day;
+    }
+
+    function getTime(date) {
+        var hours = date.getHours();
+        var daytime = (hours > 11) ? "PM" : "AM";
+        hours = hours % 12 || 12;
+        hours = hours.toString();
+        var minutes = date.getMinutes().toString();
+        var seconds = date.getSeconds().toString();
+        if (hours.length == 1) { hours = "0" + hours;}
+        if (minutes.length == 1) { minutes = "0" + minutes;}
+        if (seconds.length == 1) { seconds = "0" + seconds;}
+        return hours + "." + minutes + "." + seconds + " " + daytime;
+    }
+
     var comp = app.project.activeItem;
     var time = comp.time;
     var name = comp.name;
+    var date = new Date(Date.now());
+    var str = name + " " + getDate(date) + " " + getTime(date);
     var path = Folder.desktop.toString();
-    comp.saveFrameToPng(time, File(path + "/" + Date.now() + "_" + name + ".png"));
+    comp.saveFrameToPng(time, File(path + "/" + str + ".png"));
 })()
