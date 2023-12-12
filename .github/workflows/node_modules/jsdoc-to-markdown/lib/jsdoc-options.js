3cfa84353d845d8c73bc955cb03b3577e94ccb17
Kyle Martinez <kyletmartinez@gmail.com>
2023-12-12 11:37:51 -0600

    Test

--- .github/workflows/node_modules/jsdoc-to-markdown/lib/jsdoc-options.js
+++
@@ -1,9 +0,0 @@
-class JsdocOptions {
-  constructor (options) {
-    Object.assign(this, options)
-    this.cache = !options['no-cache']
-    delete this['no-cache']
-  }
-}
-
-module.exports = JsdocOptions
--- .github/workflows/replace.js
+++
@@ -1,6 +0,0 @@
-var Handlebars = require('handlebars')
-
-Handlebars.registerHelper('replace', function(find, replace, options) {
-    var string = options.fn(this);
-    return string.replace(new RegExp(find, 'g'), replace);
-});
