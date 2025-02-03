abff1a1b927f64cc3615a4678edd6ad87d1fd600
Kyle Martinez <kyletmartinez@gmail.com>
2025-02-03 10:11:06 -0600

    initial commit

---
+++ scripts/Add Background Layer.jsx
@@ -0,0 +1,37 @@
+/**
+ * @name Add Background Layer
+ * @version 1.0
+ * @author Kyle Martinez <www.kyle-martinez.com>
+ *
+ * @description Add a background layer to the current composition that will stay centered and match
+ * the "width" and "height" of the current composition no matter if it is resized or copy and pasted
+ * to a new composition.
+ *
+ * @license This script is provided "as is," without warranty of any kind, expressed or implied. In
+ * no event shall the author be held liable for any damages arising in any way from the use of this
+ * script.
+ *
+ * I'm just trying to help make life as an After Effects animator a little easier.
+ */
+
+(function addBackgroundLayer() {
+    app.beginUndoGroup("Add Background Layer");
+    var comp = app.project.activeItem;
+
+    var layer = comp.layers.addShape();
+    layer.name = "Background";
+
+    var position = layer.property("ADBE Transform Group").property("ADBE Position");
+    position.expression = "posterizeTime(0); [thisComp.width / 2, thisComp.height / 2];";
+
+    var group = layer.property("ADBE Root Vectors Group").addProperty("ADBE Vector Group");
+    var rectangle = group.property("ADBE Vectors Group").addProperty("ADBE Vector Shape - Rect");
+
+    var size = rectangle.property("ADBE Vector Rect Size");
+    size.setValue([comp.width, comp.height]);
+    size.expression = "posterizeTime(0); [thisComp.width, thisComp.height];";
+
+    var fill = group.property("ADBE Vectors Group").addProperty("ADBE Vector Graphic - Fill");
+    fill.property("ADBE Vector Fill Color").setValue([0.5, 0.5, 0.5]);
+    app.endUndoGroup();
+})();
---
+++ scripts/Extend All Layers.jsx
@@ -0,0 +1,36 @@
+/**
+ * @name Extend All Layers
+ * @version 1.0
+ * @author Kyle Martinez <www.kyle-martinez.com>
+ *
+ * @description Extend every layer to match the composition duration in every composition in the
+ * current project.
+ *
+ * @license This script is provided "as is," without warranty of any kind, expressed or implied. In
+ * no event shall the author be held liable for any damages arising in any way from the use of this
+ * script.
+ *
+ * I'm just trying to help make life as an After Effects animator a little easier.
+ */
+
+(function extendAllLayers() {
+
+    function extendLayers(comp) {
+        var duration = comp.duration;
+        var layers = comp.layers;
+        for (var l = comp.numLayers; l > 0; l--) {
+            layers[l].outPoint = duration;
+        }
+    }
+
+    app.beginUndoGroup("Extend All Layers");
+    var project = app.project;
+    var items = project.items;
+    for (var i = project.numItems; i > 0; i--) {
+        var item = items[i];
+        if (item instanceof CompItem) {
+            extendLayers(item);
+        }
+    }
+    app.endUndoGroup();
+})();
