10c88c8086130a8c791f1ace73d1568fe729de66
Kyle Martinez <kyletmartinez@gmail.com>
2025-02-13 12:30:09 -0600

    update layer detection

--- scripts/Separate Size Dimensions.jsx
+++ scripts/Separate Size Dimensions.jsx
@@ -1,6 +1,6 @@
 /**
  * @name Separate Size Dimensions
- * @version 1.0
+ * @version 1.1
  * @author Kyle Martinez <www.kyle-martinez.com>
  *
  * @description Separate the size dimensions for a parametric rectangle adding a slider for both
@@ -16,8 +16,8 @@
 (function separateSizeDimensions() {
 
     function getLayer(property) {
-        while (property.propertyGroup(1)) {
-            property = property.propertyGroup(1);
+        while (property.parentProperty !== null) {
+            property = property.parentProperty;
         }
         return property;
     }
