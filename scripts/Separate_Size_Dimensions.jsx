7b452c2974e97407375fddea0326ab7aecc2cef9
Kyle Martinez <kyletmartinez@gmail.com>
2025-03-20 09:51:41 -0500

    support ellipse and rectangle

--- scripts/Separate_Size_Dimensions.jsx
+++ scripts/Separate_Size_Dimensions.jsx
@@ -1,10 +1,10 @@
 /**
  * @name Separate Size Dimensions
- * @version 1.1
+ * @version 1.2
  * @author Kyle Martinez <www.kyle-martinez.com>
  *
- * @description Separate the size dimensions for a parametric rectangle adding a slider for both
- * "X Size" and "Y Size" to the layer.
+ * @description Separate the size dimensions for a parametric rectangle or ellipse by adding a
+ * slider for both "X Size" and "Y Size" to the layer.
  *
  * @license This script is provided "as is," without warranty of any kind, expressed or implied. In
  * no event shall the author be held liable for any damages arising in any way from the use of this
@@ -14,6 +14,11 @@
  */
 
 (function separateSizeDimensions() {
+
+    var matchnames = {
+        "ADBE Vector Rect Size": true,
+        "ADBE Vector Ellipse Size": true
+    };
 
     function getLayer(property) {
         while (property.parentProperty !== null) {
@@ -46,7 +51,7 @@
     var numProperties = properties.length;
     for (var p = 0; p < numProperties; p++) {
         var property = properties[p];
-        if (property.matchName === "ADBE Vector Rect Size") {
+        if (matchnames.hasOwnProperty(property.matchName) === true) {
             separateSize(property);
         }
     }
