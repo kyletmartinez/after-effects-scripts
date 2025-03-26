30e0a4cc70a4f81e7baa7ea7e9bdf24a97f06bef
Kyle Martinez <kyletmartinez@gmail.com>
2025-03-26 09:41:33 -0500

    fix flipped sides

--- scripts/Move_Parametric_Anchor_Point.jsx
+++ scripts/Move_Parametric_Anchor_Point.jsx
@@ -1,6 +1,6 @@
 /**
  * @name Move Parametric Anchor Point
- * @version 1.0
+ * @version 1.1
  * @author Kyle Martinez <www.kyle-martinez.com>
  *
  * @description Select the "Position" property of a parametric Rectangle or Ellipse and apply an
@@ -9,9 +9,9 @@
  *
  * Use the popup window to choose which side to move the Anchor Point:
  * "Top"
- * "Right"
- * "Bottom"
  * "Left"
+ * "Bottom"
+ * "Right"
  *
  * @license This script is provided "as is," without warranty of any kind, expressed or implied. In
  * no event shall the author be held liable for any damages arising in any way from the use of this
@@ -29,24 +29,21 @@
 
     var expressions = {
         "top": "[0, thisProperty.propertyGroup(1).size[1] / 2];",
-        "right": "[thisProperty.propertyGroup(1).size[0] / 2, 0];",
+        "left": "[thisProperty.propertyGroup(1).size[0] / 2, 0];",
         "bottom": "[0, thisProperty.propertyGroup(1).size[1] / -2];",
-        "left": "[thisProperty.propertyGroup(1).size[0] / -2, 0];"
+        "right": "[thisProperty.propertyGroup(1).size[0] / -2, 0];"
     };
 
     function getAnchorPointPosition() {
         var win = new Window("dialog", "Anchor Point");
         win.orientation = "column";
         win.alignChildren = ["fill", "top"];
-
         var positionOptions = ["Top", "Left", "Bottom", "Right"];
         var dropdown = win.add("dropdownlist", undefined, positionOptions);
         dropdown.selection = 0;
-
         var buttons = win.add("group");
         buttons.add("button", undefined, "Cancel", {"name": "cancel"});
         buttons.add("button", undefined, "Okay", {"name": "ok"});
-
         return (win.show() === 1) ? dropdown.selection.text : false;
     }
 
